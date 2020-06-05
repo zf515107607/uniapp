26503ba102de73b4d78515048e3ab862685e0571
FIRST_NAME LAST_NAME <515107607@qq.com>
2020-06-05 18:38:30 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,67 @@
+<template>
+	<view>
+		<view class="uni-list">
+			<view class="uni-list-cell">
+				<view class="uni-list-cell-navigate">
+					列表1		
+				</view>		
+			</view>
+			<view class="uni-list-cell">
+				<view class="uni-list-cell-navigate">
+					列表1		
+				</view>		
+			</view>
+			<view class="uni-list-cell">
+				<view class="uni-list-cell-navigate">
+					列表1		
+				</view>		
+			</view>
+		</view>
+	</view>
+	
+	<!-- <view class="content">
+		<view class="uni-list">
+			<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,index) in news" :key="index">
+				<view class="uni-media-list">
+					<image class="uni-media-list-logo" :src="item.author_avatar"></image>
+					<view class="uni-media-list-body">
+						<view class="uni-media-list-text-top">{{item.title}}</view>
+						<view class="uni-media-list-text-bottom uni-ellipsis">{{item.created_at}}</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view> -->
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				news:[]
+			}
+		},
+		// onLoad:function(){
+		// 	uni.request({
+		// 		url: 'http://unidemo.dcloud.net.cn/api/news',
+		// 		method: 'GET',
+		// 		data: {},
+		// 		success: res => {
+		// 			console.log(res)
+		// 			this.news = res.data
+		// 		},
+		// 		fail: () => {},
+		// 		complete: () => {}
+		// 	});
+		// }
+	}
+</script>
+
+<style>
+	.uni-media-list-body{
+		height: auto;	
+	}
+	.uni-media-list-text-top{
+		line-height: 1.6em;
+	}
+</style>
