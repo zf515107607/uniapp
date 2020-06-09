2f08b0d88fe20f5d2ef20e9311135c8b804cfd19
FIRST_NAME LAST_NAME <515107607@qq.com>
2020-06-09 18:15:14 +0800

    index page

---
+++ components/index/index-list.vue
@@ -0,0 +1,119 @@
+<template>
+	<view class="index-list">
+		<view class="index-list-1 u-f-acjb">
+			<view class="u-f-ac">
+				<image :src="item.userpic" mode="aspectFill" lazy-load></image>
+				{{item.username}}
+			</view>
+			<view class="u-f-ac" v-show="item.isguanzhu">
+				<view class="icon iconfont icon-21">关注</view>
+			</view>
+			
+		</view>
+		<view class="index-list-2">{{item.title}}</view>
+		<view class="index-list-3 u-f-ajc">
+				<image :src="item.titlepic" mode="widthFix" lazy-load></image>
+				<template v-if="item.type=='video'">
+					<view class="icon iconfont icon-bofang index-list-play"></view>
+					<view class="index-list-playinfo"> {{item.playnum}}次播放 {{item.long}}</view>	
+				</template>
+						
+		</view>
+		<view class="index-list-4 u-f-acjb">
+			<view class="u-fac" >
+				<view class="u-f-ac" :class="{'active':item.infonum.index ==1}"><view class="icon iconfont icon-xiaolian"></view>{{item.infonum.dingnum}}</view>
+				<view class="u-f-ac" :class="{'active':item.infonum.index ==2}"><view class="icon iconfont icon-kulian"></view>{{item.infonum.cainum}}</view>
+			</view>
+			<view class="u-fac">
+				<view class="u-f-ac"><view class="icon iconfont icon-pinglun"></view>{{item.commentnum}}</view>
+				<view class="u-f-ac"><view class="icon iconfont icon-zhuanfa"></view>{{item.sharenum}}</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			item: {
+				item: Object,
+				index:Number
+			},
+		},
+	}
+</script>
+
+<style>
+.index-list{
+		padding: 20upx;
+		border-bottom: 1upx solid #EEEEEE;
+	}
+	.index-list-1>view:first-child{
+		color: #999999;		
+	}
+	.index-list-1>view:first-child image{
+		width: 80upx;
+		height: 80upx;
+		border-radius: 100%;		
+	}
+	.index-list-1>view:last-child{
+		background-color: #F4F4F4;
+		border-radius: 5upx;
+		padding: 0 10upx;		
+	}
+	.index-list-2{
+		padding-top: 15upx;
+		font-size: 32upx;
+	}
+	.index-list-3{
+		position: relative;
+		padding-top: 15upx;
+	}
+	.index-list-play{
+		position: absolute;
+		color:#FFFFFF;
+		font-size: 140upx;
+	}
+	.index-list-playinfo{
+		position: absolute;
+		background:rgba(51,51,51,0.72);
+		color: #FFFFFF;
+		bottom:8upx ;
+		right: 8upx;
+		border-radius: 40upx;
+		font-size: 25upx;
+		padding:0 12upx ;
+	}
+	
+	.index-list-3>image{
+		width: 100%;
+		border-radius: 20upx;
+	}
+	.index-list-4{
+		padding: 15upx 0;
+	}
+	.index-list-4 view{
+		color: #999999;	
+	}
+	.index-list-4>view>view{
+		display: flex;
+		align-items: center;		
+	}
+	.index-list-4>view:first-child{
+		display: flex;
+		align-items: center;		
+	}
+	.index-list-4>view:last-child{
+		display: flex;
+		align-items: center;		
+	}
+	.index-list-4>view>view:first-child{
+		margin-right:10upx ;
+	}
+	.index-list-4>view>view>view{
+		margin-right: 10upx;		
+	}
+	.index-list-4 .active,.index-list-4 .active view{
+		color: #c5f61c;
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,67 +1,279 @@
 <template>
 	<view>
-		<view class="uni-list">
-			<view class="uni-list-cell">
-				<view class="uni-list-cell-navigate">
-					列表1		
-				</view>		
-			</view>
-			<view class="uni-list-cell">
-				<view class="uni-list-cell-navigate">
-					列表1		
-				</view>		
-			</view>
-			<view class="uni-list-cell">
-				<view class="uni-list-cell-navigate">
-					列表1		
-				</view>		
-			</view>
+		<tab-bar-head :tabBars="tabBars" :tabIndex="tabIndex" @tabtap="tabtap"></tab-bar-head>
+		<view >
+			<swiper class="swiper-box" :style="{height:swiperheight+'px'}" :current="tabIndex" @change="tabChange">
+				<swiper-item v-for="(items,index) in newslist" :key="index">
+					<scroll-view scroll-y="true" class="list">
+						<block v-for="(item,index1) in items.list" :key="index1">
+							<index-list :item="item" :index="index"></index-list>
+						</block>
+					</scroll-view>
+				</swiper-item>
+
+			</swiper>
 		</view>
 	</view>
-	
-	<!-- <view class="content">
-		<view class="uni-list">
-			<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,index) in news" :key="index">
-				<view class="uni-media-list">
-					<image class="uni-media-list-logo" :src="item.author_avatar"></image>
-					<view class="uni-media-list-body">
-						<view class="uni-media-list-text-top">{{item.title}}</view>
-						<view class="uni-media-list-text-bottom uni-ellipsis">{{item.created_at}}</view>
-					</view>
-				</view>
-			</view>
-		</view>
-	</view> -->
 </template>
 
 <script>
+	import indexList from '../../components/index/index-list.vue'
+	import tabBarHead from '../../components/index/tab-bar-head.vue'
+
 	export default {
+		components: {
+			indexList,
+			tabBarHead
+		},
 		data() {
 			return {
-				news:[]
+				swiperheight: 500,
+				tabIndex: 0,
+				tabBars: [{
+						name: "关注",
+						id: "guanzhu"
+					},
+					{
+						name: "推荐",
+						id: "tuijian"
+					},
+					{
+						name: "体育",
+						id: "tiyu"
+					},
+					{
+						name: "热点",
+						id: "redian"
+					},
+					{
+						name: "娱乐",
+						id: "yule"
+					}
+				],
+				newslist: [
+					{
+						list: [
+							{
+									userpic: "../../static/pic/baishen.png",
+									username: "昵称",
+									isguanzhu: false,
+									title: "标题",
+									titlepic: "../../static/pic/beijita.png",
+									type: "img", // img:图片，video:视频
+									playnum: "20w",
+									long: "2:17",
+									infonum: {
+										index: 1, // 0:没有操作, 1:顶，2：踩
+										dingnum: 11,
+										cainum: 11,
+									},
+									cai: {},
+									commentnum: 20,
+									sharenum: 10
+								},
+								{
+									userpic: "../../static/pic/baishen.png",
+									username: "昵称",
+									isguanzhu: true,
+									title: "标题",
+									titlepic: "../../static/pic/beijita.png",
+									type: "video", // img:图片，video:视频
+									playnum: "20w",
+									long: "2:17",
+									infonum: {
+										index: 2, // 0:没有操作, 1:顶，2：踩
+										dingnum: 11,
+										cainum: 11,
+									},
+									cai: {},
+									commentnum: 20,
+									sharenum: 10
+								}
+						]
+					},
+					{
+						list: [
+							{
+									userpic: "../../static/pic/baishen.png",
+									username: "昵称",
+									isguanzhu: false,
+									title: "标题",
+									titlepic: "../../static/pic/baishen.png",
+									type: "img", // img:图片，video:视频
+									playnum: "20w",
+									long: "2:17",
+									infonum: {
+										index: 1, // 0:没有操作, 1:顶，2：踩
+										dingnum: 11,
+										cainum: 11,
+									},
+									cai: {},
+									commentnum: 20,
+									sharenum: 10
+								},
+								{
+									userpic: "../../static/pic/baishen.png",
+									username: "昵称",
+									isguanzhu: true,
+									title: "标题",
+									titlepic: "../../static/pic/beijita.png",
+									type: "video", // img:图片，video:视频
+									playnum: "20w",
+									long: "2:17",
+									infonum: {
+										index: 2, // 0:没有操作, 1:顶，2：踩
+										dingnum: 11,
+										cainum: 11,
+									},
+									cai: {},
+									commentnum: 20,
+									sharenum: 10
+								}
+						]
+					},
+					{
+						list: [
+							{
+									userpic: "../../static/pic/baishen.png",
+									username: "昵称",
+									isguanzhu: false,
+									title: "标题",
+									titlepic: "../../static/pic/beijita.png",
+									type: "img", // img:图片，video:视频
+									playnum: "20w",
+									long: "2:17",
+									infonum: {
+										index: 1, // 0:没有操作, 1:顶，2：踩
+										dingnum: 11,
+										cainum: 11,
+									},
+									cai: {},
+									commentnum: 20,
+									sharenum: 10
+								},
+								{
+									userpic: "../../static/pic/baishen.png",
+									username: "昵称",
+									isguanzhu: true,
+									title: "标题",
+									titlepic: "../../static/pic/baishen.png",
+									type: "video", // img:图片，video:视频
+									playnum: "20w",
+									long: "2:17",
+									infonum: {
+										index: 2, // 0:没有操作, 1:顶，2：踩
+										dingnum: 11,
+										cainum: 11,
+									},
+									cai: {},
+									commentnum: 20,
+									sharenum: 10
+								}
+						]
+					},
+					{
+						list: [
+							{
+									userpic: "../../static/pic/baishen.png",
+									username: "昵称",
+									isguanzhu: false,
+									title: "标题",
+									titlepic: "../../static/pic/baishen.png",
+									type: "img", // img:图片，video:视频
+									playnum: "20w",
+									long: "2:17",
+									infonum: {
+										index: 1, // 0:没有操作, 1:顶，2：踩
+										dingnum: 11,
+										cainum: 11,
+									},
+									cai: {},
+									commentnum: 20,
+									sharenum: 10
+								},
+								{
+									userpic: "../../static/pic/baishen.png",
+									username: "昵称",
+									isguanzhu: true,
+									title: "标题",
+									titlepic: "../../static/pic/beijita.png",
+									type: "video", // img:图片，video:视频
+									playnum: "20w",
+									long: "2:17",
+									infonum: {
+										index: 2, // 0:没有操作, 1:顶，2：踩
+										dingnum: 11,
+										cainum: 11,
+									},
+									cai: {},
+									commentnum: 20,
+									sharenum: 10
+								}
+						]
+					},
+					{
+						list: [
+							{
+									userpic: "../../static/pic/baishen.png",
+									username: "昵称",
+									isguanzhu: false,
+									title: "标题",
+									titlepic: "../../static/pic/baishen.png",
+									type: "img", // img:图片，video:视频
+									playnum: "20w",
+									long: "2:17",
+									infonum: {
+										index: 1, // 0:没有操作, 1:顶，2：踩
+										dingnum: 11,
+										cainum: 11,
+									},
+									cai: {},
+									commentnum: 20,
+									sharenum: 10
+								},
+								{
+									userpic: "../../static/pic/baishen.png",
+									username: "昵称",
+									isguanzhu: true,
+									title: "标题",
+									titlepic: "../../static/pic/baishen.png",
+									type: "video", // img:图片，video:视频
+									playnum: "20w",
+									long: "2:17",
+									infonum: {
+										index: 2, // 0:没有操作, 1:顶，2：踩
+										dingnum: 11,
+										cainum: 11,
+									},
+									cai: {},
+									commentnum: 20,
+									sharenum: 10
+								}
+						]
+					}
+				]
 			}
 		},
-		// onLoad:function(){
-		// 	uni.request({
-		// 		url: 'http://unidemo.dcloud.net.cn/api/news',
-		// 		method: 'GET',
-		// 		data: {},
-		// 		success: res => {
-		// 			console.log(res)
-		// 			this.news = res.data
-		// 		},
-		// 		fail: () => {},
-		// 		complete: () => {}
-		// 	});
-		// }
+		methods: {
+			tabtap(index) {
+				this.tabIndex = index
+			},
+			tabChange(e){
+				this.tabIndex = e.detail.current
+			}
+		},
+		onLoad() {
+			uni.getSystemInfo({
+				success: (res) => {
+					let height=res.windowHeight-uni.upx2px(100)
+					this.swiperheight = height
+				}
+			})
+		}
+
 	}
 </script>
 
 <style>
-	.uni-media-list-body{
-		height: auto;	
-	}
-	.uni-media-list-text-top{
-		line-height: 1.6em;
-	}
+	
 </style>
